e7847f6fdcaa2d7560f28f13b5a469d160e2b4e2
bidyuttalukdar <bidyut3004@gmail.com>
2023-07-06 16:42:49 +0530

    1207. Unique Number of Occurrences is added 2

--- 1207_Unique_Number_of_Occurrences.js
+++ 1207_Unique_Number_of_Occurrences.js
@@ -27,3 +27,24 @@ Constraints:
 
 
 
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+ var uniqueOccurrences = function(arr) {
+    let map = new Map();
+    for(let i=0;i<arr.length;i++){
+        if(map.has(arr[i])){
+            map.set(arr[i],map.get(arr[i])+1)
+        }else{
+            map.set(arr[i],1)
+        }
+    }
+ 
+    for(let [key,value] of map){
+        if(value>1){
+            return true
+        }
+    }
+    return false
+};
